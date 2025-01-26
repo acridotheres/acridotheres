2f094380cf8d7d8dcd558c0c02aed6a01c88cd5c
Leonard Lesinski <84378319+Le0X8@users.noreply.github.com>
2025-01-26 12:55:30 +0100

    feat: Added Tauri codebase

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { invoke } from '@tauri-apps/api/core';
+
+  async function greet(event: Event) {
+    event.preventDefault();
+    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+    alert(await invoke('greet', { name: 'test' }));
+  }
+</script>
+
+<div class="flex flex-col items-center justify-center">
+  <h1 class="text-4xl font-bold">Hello, world!</h1>
+  <button on:click={greet} class="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md">Greet</button>
+</div>
